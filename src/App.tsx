f9dcdc5e4f1b8206ffcb820bc2bdc5f775422967
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-12-29 07:26:01 -0500

    Hotfixed import statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {ReactNode, useState} from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home.tsx";
+import Home from "./Home.tsx";
 function App() {
 
   return (
