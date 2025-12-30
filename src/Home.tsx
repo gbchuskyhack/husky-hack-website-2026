ab9590d5ff3e0d49926822007232f68ceaa0fc9b
Sokmontrey <sythatsokmontrey@gmail.com>
2025-12-30 16:15:51 -0500

    feat: add FooterSection component with social links, integrate it into the homepage, and include icon dependencies and smooth scroll.

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,6 +4,7 @@ import AboutUsSection from "./components/landing-page-sections/AboutUsSection";
 import SponsorsSection from "./components/landing-page-sections/SponsorsSection";
 import FAQSection from "./components/landing-page-sections/FAQSection";
 import TeamSection from "./components/landing-page-sections/TeamSection";
+import FooterSection from "./components/landing-page-sections/FooterSection";
 
 export default function Home() {
     return (
@@ -16,7 +17,7 @@ export default function Home() {
             <FAQSection />
             <TeamSection />
             {/* Contact Section */}
-            {/* Footer */}
+            <FooterSection />
         </>
     )
 }
