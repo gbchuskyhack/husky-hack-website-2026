fa58f7eb840a2ed59c64aa9587eb3a4ec99d316f
Sokmontrey <sythatsokmontrey@gmail.com>
2026-01-02 13:45:42 -0500

    feat: move ContactSection to a dedicated page and update TeamMarquee styling

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import ContactSection from "../../components/landing-page-sections/ContactSection.tsx";
+
+export default function Contact() {
+    return (
+        <div>
+            <ContactSection />
+        </div>
+    );
+}
