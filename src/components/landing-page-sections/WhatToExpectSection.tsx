e903d62eb9deff7d4338a53510b193e2c76ca523
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2026-01-26 19:27:46 -0500

    Added a Wrapper for the caves to have multiple secctions with same background

--- src/components/landing-page-sections/WhatToExpectSection.tsx
+++ src/components/landing-page-sections/WhatToExpectSection.tsx
@@ -61,30 +61,8 @@ const RoleCard = ({ image = "/huskyhacklogo.svg", title, description, buttonText
 
 const WhatToExpectSection = () => {
     return (
-        <div className="relative py-12 md:py-48 px-4 sm:px-8 bg-[linear-gradient(to_bottom,#1C6D41,#243B5C,#090F18,#030609)] text-white overflow-hidden min-h-screen h-auto pb-32">
-            {/* Background  */}
-            <div className="absolute top-0 left-0 h-full w-auto z-0 pointer-events-none opacity-30 lg:opacity-100">
-                <Image
-                    src={"/expectations/left_cave.svg"}
-                    alt="Cave Left"
-                    className="h-full w-auto object-cover md:max-w-none"
-                    width={500}
-                    height={1000}
-                    unoptimized
-                />
-            </div>
-
-            <div className="absolute top-0 right-0 h-full w-auto z-0 pointer-events-none opacity-30 lg:opacity-100">
-                <Image
-                    src={"/expectations/right_cave.svg"}
-                    alt="Cave Right"
-                    className="h-full w-auto object-cover md:max-w-none"
-                    width={500}
-                    height={1000}
-                    unoptimized
-                />
-            </div>
-
+        <div>
+          
             <h1 className="relative z-10 text-3xl md:text-4xl font-bold text-center mt-8 md:mt-12">What to Expect</h1>
 
             <div className="flex flex-col justify-evenly gap-32 md:gap-24 mt-24 md:mt-12">
