10176047a3352183bc50cba44ebfc7d5a1b1828e
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2026-01-26 20:39:39 -0500

    Changed background and removed the husky doodle

--- src/components/landing-page-sections/FAQSection.tsx
+++ src/components/landing-page-sections/FAQSection.tsx
@@ -5,29 +5,20 @@ import Image from "next/image";
 
 export default function FAQSection() {
     return (
-        <section id="FAQ" className="py-40 pt-10">
+        <section id="FAQ" className="py-40 pt-10 bg-[#1C6D41]">
             <h1 className="text-4xl font-serif text-center mb-10">FAQs</h1>
 
             <div className="flex flex-col justify-center items-center md:flex-row-reverse gap-4 w-full ">
-                <div className="justify-self-center md:justify-self-end self-center md:self-start flex flex-col items-center justify-center">
-                    <h1 className="text-2xl font-serif">?</h1>
-                    <Image
-                        src={huskyPeekComputer}
-                        alt="Husky Peek Using Computer"
-                        width={300}
-                        height={300}
-                        className="w-1/2"
-                    />
-                </div>
-
-                <div className="flex flex-col gap-4 w-[50vw] items-center">
+           
+                <div className="flex flex-col gap-4 w-[50vw] items-start">
                     {faqs.map((faq, index) => (
-                        <Accordion
 
+                        <div className="bg-[#172334] rounded-lg w-[50vw] text-white">
+                           <Accordion
                             key={index}
                             question={faq.question}
-                            answer={faq.answer}
-                        />
+                            answer={faq.answer}                        />
+                        </div>
                     ))}
                 </div>
             </div>
